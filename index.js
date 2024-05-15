ae8640f01f1eba09f195f0b9722e4a83b3d4e9b1
Liza Tarakanova <liztarakanova@mail.ru>
2024-05-15 13:05:28 +0500

    input search

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 let search = document.querySelector('.search__icon_header');
 let searchBody = document.querySelector('.search__body');
 let searchButton = document.querySelector('.search__button');
+let searchClose = document.querySelector('.search__close');
+let input = document.querySelector('.search__input');
 
 search.onclick = () => {
+    searchBody.style.transition = '1s';
     searchBody.style.display = 'block';
     search.replaceWith(searchBody);
+    input.style.color = '#D1D1E5';
 }
 
-search.blur = () => {
-    searchBody.style.display = '';
-    searchBody.prepend.search;
+input.onfocus = () => {
+    input.value = '';
+    input.style.color = 'black';
 }
 
+searchClose.onclick = () => {
+    input.style.color = '#D1D1E5';
+    input.value = 'Найти';
+}
+
+searchBody.onblur = () => {
+    if (input.value === '') {
+        input.style.color = '#D1D1E5';
+        input.value = 'Найти';
+    }
+}
